bb7c188d0cc5f47292625585a80960f96b5c1954
garcinxen <anguila3000@hotmail.com>
2023-02-18 15:59:06 +0000

    05_Modelo_Eventos_UI5

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,11 +1,55 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
+     * @param {typeof sap.ui.model.Filter} Filter
+     * @param {typeof sap.ui.model.FilterOperator} FilterOperator
      */
-    function (Controller) {
+    function (Controller, Filter, FilterOperator) {
         "use strict";
+
+        function onInit () {
+            var oJSONModel = new sap.ui.model.json.JSONModel();
+            var oView = this.getView();
+            //var i18nBundle = oView.getModel("i18n").getResourceBundle();
+            
+            oJSONModel.loadData("./localService/mockdata/Employees.json");
+            // oJSONModel.attachRequestCompleted(function(oEventModel){
+            //     console.log(JSON.stringify(oJSONModel.getData()));
+            // });
+            oView.setModel(oJSONModel);
+        };
+
+        function onFilter () {
+            var oJSON = this.getView().getModel().getData();
+            var filter = [];
+
+            if (oJSON.EmployeeId !== "") {
+                filter.push(new Filter("EmployeeID", FilterOperator.EQ, oJSON.EmployeeId));
+            }
+
+            if (oJSON.CountryKey !== "") {
+                filter.push(new Filter("Country", FilterOperator.EQ, oJSON.CountryKey));
+            }
+
+            var oList = this.getView().byId("tableEmployee");
+            var oBinding = oList.getBinding("items");
+            oBinding.filter(filter);
+        };
+
+        function onClearFilter () {
+            var oModel = this.getView().getModel();
+            oModel.setProperty("/EmployeeId", "");
+            oModel.setProperty("/CountryKey", "");
+
+            var filter = [];
+            var oList = this.getView().byId("tableEmployee");
+            var oBinding = oList.getBinding("items");
+            oBinding.filter(filter);
+        };
 
         var Main = Controller.extend("logaligroup.employees.controller.MainView", {});
             
@@ -23,5 +67,9 @@ sap.ui.define([
                 this.getView().byId("slCountry").setVisible(false);
             }
         };
+
+        Main.prototype.onInit = onInit;
+        Main.prototype.onFilter = onFilter;
+        Main.prototype.onClearFilter = onClearFilter;
         return Main;        
     });
