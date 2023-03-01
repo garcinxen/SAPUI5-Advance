c2e5ca54f25e3367207b3ea955f677ece4b1f431
garcinxen <anguila3000@hotmail.com>
2023-03-01 20:35:34 +0000

    05_Listas

--- lists/webapp/controller/App.controller.js
+++ lists/webapp/controller/App.controller.js
@@ -1,3 +1,4 @@
+//@ts-nocheck
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ],
@@ -12,6 +13,78 @@ sap.ui.define([
                 let oJSONModel = new sap.ui.model.json.JSONModel();
                 oJSONModel.loadData("../localService/mockdata/ListData.json");
                 this.getView().setModel(oJSONModel);
+            },
+
+            getGroupHeader: function (oGroup) {
+                let groupHeaderListItem = new sap.m.GroupHeaderListItem({
+                    title: oGroup.key,
+                    upperCase: true
+                });
+
+                return groupHeaderListItem;
+            },
+
+            onShowSelectedItems: function () {
+                let standardList = this.getView().byId("standardList");
+                let selectedItems = standardList.getSelectedItems();
+                let i18nModel = this.getView().getModel("i18n").getResourceBundle();
+
+                if (selectedItems.length === 0) {
+                   sap.m.MessageToast.show(i18nModel.getText("noSelection")); 
+                } else {
+                    let textMessage = i18nModel.getText("selection");
+
+                    for (let item in selectedItems) {
+                        let context = selectedItems[item].getBindingContext();
+                        let oContext = context.getObject();
+                        textMessage = textMessage + " - " + oContext.Material;
+                    }
+
+                    sap.m.MessageToast.show(textMessage);
+                }
+            },
+
+            onDeleteSelectedItems: function () {
+                let standardList = this.getView().byId("standardList");
+                let selectedItems = standardList.getSelectedItems();
+                let i18nModel = this.getView().getModel("i18n").getResourceBundle();
+
+                if (selectedItems.length === 0) {
+                   sap.m.MessageToast.show(i18nModel.getText("noSelection")); 
+                } else {
+                    let textMessage = i18nModel.getText("delete");
+                    let model = this.getView().getModel();
+                    let products = model.getProperty("/Products");
+
+                    let arrayId = [];
+
+                    for (let i in selectedItems) {
+                        let context = selectedItems[i].getBindingContext();
+                        let oContext = context.getObject();
+
+                        arrayId.push(oContext.Id);
+                        textMessage = textMessage + " - " + oContext.Material;
+                    }
+
+                    products = products.filter(function (p) {
+                        return !arrayId.includes(p.Id);
+                    });
+
+                    model.setProperty("/Products", products);
+                    standardList.removeSelections();
+                    sap.m.MessageToast.show(textMessage);
+                }
+            },
+
+            onDeleteRow: function (oEvent) {
+                let selectedRow = oEvent.getParameter("listItem");
+                let context = selectedRow.getBindingContext();
+                let splitPath = context.getPath().split("/");
+                let indexSeletedRow = splitPath[splitPath.length-1];
+                let model = this.getView().getModel();
+                let products = model.getProperty("/Products");
+                products.splice(indexSeletedRow, 1);
+                model.refresh();
             }
         });
     });
